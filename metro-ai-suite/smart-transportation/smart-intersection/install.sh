4013713daf1dfbd71c4497d1ffccaa9e5ae6cb1f
Vaghesh <vaghesh.patel@intel.com>
2025-07-03 04:07:02 +0530

    Updated the name to Metro Vision AI App Recipe

--- metro-ai-suite/smart-transportation/smart-intersection/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-docker run --rm -ti \
-    -e http_proxy -e https_proxy -e no_proxy \
-    -v $(pwd)/init.sh:/init.sh \
-    -v $(pwd)/chart:/chart \
-    -v $(pwd)/src:/src \
-    docker.io/library/python:3.12 bash init.sh
